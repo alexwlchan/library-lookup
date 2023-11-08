6066a05619a834078396a022993bcdc878653d1c
Alex Chan <alex@alexwlchan.net>
2023-11-08 23:21:47 +0000

    Get more data in get_book_data.py

--- get_book_data.py
+++ get_book_data.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import base64
 import datetime
-import io
+import functools
 import json
 import os
 import re
@@ -14,46 +13,6 @@ import keyring
 import mechanize
 import tqdm
 from unidecode import unidecode
-from PIL import Image
-
-from concurrently import concurrently
-
-
-def isLoginForm(form):
-    return form.attrs.get("id") == "frmLogin"
-
-
-def get_all_books_in_default_list(browser, *, base_url, username, password):
-    browser.open(base_url).read()
-
-    browser.select_form(predicate=isLoginForm)
-    browser.set_value(username, name="BRWLID")
-    browser.set_value(password, name="BRWLPWD")
-    browser.submit().read()
-
-    browser.follow_link(text="Dashboard").read()
-    browser.follow_link(text="View all saved lists").read()
-    response = browser.follow_link(text="Default").read()
-
-    while True:
-        soup = bs4.BeautifulSoup(response, "html.parser")
-
-        result_list = soup.find("div", attrs={"id": "result-content-list"})
-
-        yield from result_list.find_all("fieldset")
-
-        pagination_nav = soup.find("nav", attrs={"class": "result-pages-prvnxt"})
-
-        try:
-            next_page = next(
-                anchor
-                for anchor in pagination_nav.find_all("a")
-                if "Next page of search results" in anchor.text
-            )
-
-            response = browser.open(base_url + next_page.attrs["href"])
-        except StopIteration:
-            break
 
 
 def slugify(u):
@@ -69,11 +28,11 @@ def slugify(u):
     return a
 
 
-def save_image_locally(fieldset_soup):
+def save_image_locally(img_element):
     """
     Downloads a local copy of an image, and returns the path.
     """
-    image_url = fieldset_soup.find("img").attrs["longdesc"]
+    image_url = img_element.attrs["longdesc"]
     url = hyperlink.URL.from_text(image_url)
 
     # We're going to save it to the 'covers' directory, which is gitignore'd.
@@ -85,7 +44,7 @@ def save_image_locally(fieldset_soup):
         isbn = url.get("ISBN")[0]
         existing_image = next(p for p in os.listdir("covers") if p.startswith(isbn))
         return os.path.join("covers", existing_image)
-    except (IndexError, StopIteration) as e:
+    except (IndexError, StopIteration):
         pass
 
     # By default the library website returns a small image, but we can futz
@@ -109,77 +68,300 @@ def save_image_locally(fieldset_soup):
     return out_path
 
 
-def get_book_info(fieldset, *, base_url, browser):
-    body = fieldset.find("div", attrs={"class": "card-body"})
-    anchor_tag = body.find("a")
-    relative_link = anchor_tag.attrs["href"]
-    title = anchor_tag.find("span").text
+class LibraryBrowser:
+    def __init__(self, *, base_url: str, username: str, password: str) -> None:
+        self.base_url = base_url
+        self.browser = mechanize.Browser()
 
-    image = save_image_locally(fieldset)
+        self._configure_browser(username=username, password=password)
 
-    details = fieldset.find("div", attrs={"class": "recdetails"})
-    author = details.find("span", attrs={"class": "d-block"}).text
-
-    year = next(
-        s.text
-        for s in details.find_all("span", attrs={"class": "d-block"})
-        if s.text.isnumeric()
-    )
-
-    try:
-        summary = fieldset.find("div", attrs={"class": "summary"}).text
-    except AttributeError:
-        summary = ""
-
-    availability = (
-        fieldset.find("div", attrs={"class": "availability"}).find("a").attrs["href"]
-    )
-
-    availability_resp = browser.open(base_url + availability).read()
-
-    availability_info = list(get_availability_info(availability_resp))
-
-    return {
-        "image": image,
-        "title": title,
-        "author": author,
-        "url": base_url + relative_link,
-        "summary": summary,
-        "year": year,
-        "availability_info": availability_info,
-    }
-
-
-def get_availability_info(html):
-    soup = bs4.BeautifulSoup(html, "html.parser")
-
-    # There's a table with four columns:
-    # Location/Collection/Call number/Status
-    #
-    # I'm mainly interested in location and status
-    for row in soup.find("table").find("tbody").find_all("tr"):
-        fields = [
-            ("location", "Location"),
-            ("collection", "Collection"),
-            ("status", "Status/Desc"),
-            ("call_number", "Call number"),
-        ]
-
-        info = {
-            key: row.find("td", attrs={"data-caption": caption}).text
-            if row.find("td", attrs={"data-caption": caption})
-            else ""
-            for (key, caption) in fields
-        }
-
-        if "(Hertfordshire County Council)" not in info["location"]:
-            continue
-
-        info["location"] = info["location"].replace(
-            " (Hertfordshire County Council)", ""
+    def _configure_browser(self, *, username: str, password: str) -> None:
+        """
+        Set up the browser, and log in to the library website.
+        """
+        self.browser.set_handle_robots(False)
+        self.browser.set_handle_redirect(True)
+        self.browser.set_handle_refresh(
+            mechanize._http.HTTPRefreshProcessor(), max_time=1, honor_time=True
         )
 
-        yield info
+        self.browser.open(self.base_url).read()
+
+        self.browser.select_form(
+            predicate=lambda form: form.attrs.get("id") == "frmLogin"
+        )
+        self.browser.set_value(username, name="BRWLID")
+        self.browser.set_value(password, name="BRWLPWD")
+        self.browser.submit().read()
+
+    def _get_soup(self, url: str):
+        """
+        Open a URL and parse the HTML with BeautifulSoup.
+        """
+        if url.startswith("/"):
+            resp = self.browser.open(self.base_url + url)
+        else:
+            resp = self.browser.open(url)
+
+        return bs4.BeautifulSoup(resp, "html.parser")
+
+    @functools.cache
+    def get_default_list(self):
+        """
+        Returns the URL to my default list.
+        """
+        # Go to the homepage
+        self.browser.open(self.base_url)
+
+        # In the top right-hand corner is a dropdown menu; one of the
+        # items is a link to "Dashboard".  Click it.
+        self.browser.follow_link(text="Dashboard")
+
+        # On the left-hand side is a list of links titled "My account".
+        # One of the items is a link to my saved lists.  Click it.
+        resp = self.browser.follow_link(text="View all saved lists")
+
+        # Finally, a table which has my lists.  There's only one, which
+        # is titled "Default".  Make a note of the URL and the title count.
+        soup = bs4.BeautifulSoup(resp, "html.parser")
+        count = int(soup.find("td", attrs={"data-caption": "Titles"}).text)
+
+        url = self.browser.find_link(text="Default").absolute_url
+
+        return {"count": count, "url": url}
+
+    def get_books_in_list(self, url) -> None:
+        """
+        Generate a list of books in a list, which is all the books
+        I've marked with a bookmark icon.
+        """
+        # This is the first page of results, but there are more pages.
+        # Go through each page in turn, and look for data about the books.
+        while True:
+            soup = self._get_soup(url)
+
+            # The books on the page are stored in the following structure:
+            #
+            #     <div id="result-content-list" …>
+            #       <fieldset class="card card-list">
+            #         … info about book 1 …
+            #       </fieldset>
+            #       <fieldset class="card card-list">
+            #         … info about book 2 …
+            #       </fieldset>
+            #       …
+            #
+            result_content_List = soup.find("div", attrs={"id": "result-content-list"})
+
+            for fieldset in result_content_List.find_all("fieldset"):
+                yield self.parse_fieldset_info(fieldset)
+
+            # Now look for a link to the next page, if there is one.
+            #
+            #     <nav class="prvnxt result-pages-prvnxt">
+            #       <ul class="list-inline mb-0">
+            #         <li class="list-inline-item prv">Previous</li>
+            #         <li class="list-inline-item nxt">
+            #           <a href="/cgi-bin/spydus.exe/…">Next</a>
+            #         </li>
+            #
+            pagination_nav = soup.find("nav", attrs={"class": "result-pages-prvnxt"})
+            next_link = pagination_nav.find("li", attrs={"class": "nxt"}).find("a")
+
+            if next_link is None:
+                break
+
+            url = next_link.attrs["href"]
+
+    def parse_fieldset_info(self, fieldset):
+        """
+        Given a <fieldset> element from the list of books in a saved list,
+        return all the metadata I want to extract.
+        """
+        title_elem = fieldset.find("h2", attrs={"class": "card-title"})
+        title = title_elem.getText()
+
+        url = title_elem.find("a").attrs["href"]
+        record_details = self.get_record_details(url)
+
+        image = save_image_locally(fieldset.find("img"))
+
+        # The author and publication year are in a block like so:
+        #
+        #     <div class="card-text recdetails">
+        #       <span class="d-block">Cleeves, Ann</span>
+        #       <span class="d-block">2023</span>
+        #     </div>
+        #
+        recdetail_div = fieldset.find("div", attrs={"class": "recdetails"})
+        recdetail_spans = recdetail_div.find_all("span")
+
+        if title == "Ursula K. Le Guin : voyager to inner lands and to outer space" and len(recdetail_spans) == 1:
+            author = "Ursula K. Le Guin"
+            title = "Voyager to Inner Lands and to Outer Space"
+            publication_year = "1979"
+        elif len(recdetail_spans) != 2:
+            print(
+                f'Unexpected data on {title}; could not find two instances of <div class="recdetails">'
+            )
+            author = None
+            publication_year = None
+        else:
+            author = recdetail_spans[0].getText()
+            publication_year = recdetail_spans[1].getText()
+
+        # The summary is in a block like:
+        #
+        #     <div class="card-text summary">
+        #       When Jem Rosco - sailor, adventurer and local legend -
+        #       …
+        #     </div>
+        #
+        # Note that not all books have a summary, in which case this
+        # element is missing.
+        summary_elem = fieldset.find("div", attrs={"class": "summary"})
+        if summary_elem is None:
+            summary = None
+        else:
+            summary = summary_elem.getText().strip()
+
+        # There's a link to the availability popover:
+        #
+        #     <div class="card-text availability">
+        #       …
+        #       <a href="/cgi-bin/spydus.exe/XHLD/WPAC/…" …>
+        #         View availability
+        #       </a>
+        #
+        # That's the URL we need to open to get availability info.
+        availability_elem = fieldset.find("div", attrs={"class": "availability"})
+        availability_url = availability_elem.find("a").attrs["href"]
+        availability = self.get_availability_info(availability_url)
+
+        return {
+            "title": title,
+            "record_details": record_details,
+            "image": image,
+            "author": author,
+            "publication_year": publication_year,
+            "summary": summary,
+            "availability": availability,
+        }
+
+    def get_record_details(self, url: str) -> str:
+        """
+        Given the URL to a book's page in the current browser session,
+        get all the record details, which are shown as a table on the page.
+
+        Of particular interest here is the bookmark link, which should work
+        across sessions.  For some reason, the Spydus URLs you get when
+        logged in are tied to your current session, and don't always work later.
+
+        Sometimes you get an error like:
+
+            Session must be logged in to display this page
+
+        even if you're already logged in!
+
+        I don't use much of this right now, but while I'm in this table it
+        makes sense to grab it all and work out what to do with it later.
+        """
+        soup = self._get_soup(url)
+
+        # This is the rough structure of the HTML that renders the
+        # "Record details" table (or at least the bits we care about):
+        #
+        #       <div id="tabRECDETAILS-body" …>
+        #         <div class="row">
+        #           <div class="col-sm-3 col-md-3 fd-caption">
+        #             <span>Main title:</span>
+        #           </div>
+        #           <div class="col pl-sm-0">
+        #             <span class="d-block"><a href="/cgi-bin/spydus.exe/ENQ/…"><span>A Cuban girl's guide to tea and tomorrow</span></a> / Laura Taylor Namey.</span>
+        #           </div>
+        #         </div>
+        #         <div class="row">
+        #           <div class="col-sm-3 col-md-3 fd-caption">
+        #             <span>Imprint:</span>
+        #           </div>
+        #           <div class="col pl-sm-0">
+        #             <span class="d-block">London : Simon & Schuster, 2022.</span>
+        #           </div>
+        #         </div>
+        #         …
+        #
+        rec_details_body = soup.find("div", attrs={"id": "tabRECDETAILS-body"})
+
+        record_details = {}
+
+        for row in rec_details_body.find_all("div", attrs={"class": "row"}):
+            caption_text = row.find("div", attrs={"class": "fd-caption"}).getText()
+
+            # Strip the trailing colon
+            key = re.sub(r":$", "", caption_text.strip())
+
+            # There may be multiple rows, e.g. "Subject" and "Dewey Class"
+            value = [
+                span.getText().strip()
+                for span in row.find_all("span", attrs={"class": "d-block"})
+            ]
+
+            # If there's a single value and we're not expecting multiple
+            # values in this field, just get the single value.
+            if key not in {
+                "Added title",
+                "Author",
+                "Dewey class",
+                "Language",
+                "Local class",
+                "More Information",
+                "Notes",
+                "Series title",
+                "Subject",
+            }:
+                if len(value) == 1:
+                    value = value[0]
+                else:
+                    print(f"Record detail had multiple entries for {key}: {url}")
+
+            record_details[key] = value
+
+        return record_details
+
+    def get_availability_info(self, availability_url):
+        soup = self._get_soup(availability_url)
+
+        availability = []
+
+        # There's a table with four columns:
+        # Location/Collection/Call number/Status
+        #
+        # I'm mainly interested in location and status
+        for row in soup.find("table").find("tbody").find_all("tr"):
+            fields = [
+                ("location", "Location"),
+                ("collection", "Collection"),
+                ("status", "Status/Desc"),
+                ("call_number", "Call number"),
+            ]
+
+            info = {
+                key: row.find("td", attrs={"data-caption": caption}).text
+                if row.find("td", attrs={"data-caption": caption})
+                else ""
+                for (key, caption) in fields
+            }
+
+            if "(Hertfordshire County Council)" not in info["location"]:
+                continue
+
+            info["location"] = info["location"].replace(
+                " (Hertfordshire County Council)", ""
+            )
+
+            availability.append(info)
+
+        return availability
 
 
 if __name__ == "__main__":
@@ -190,37 +372,20 @@ if __name__ == "__main__":
         username = keyring.get_password("library", "username")
         password = keyring.get_password("library", "password")
 
-    base_url = "https://herts.spydus.co.uk"
-
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-    browser.set_handle_redirect(True)
-    browser.set_handle_refresh(
-        mechanize._http.HTTPRefreshProcessor(), max_time=1, honor_time=True
+    browser = LibraryBrowser(
+        base_url="https://herts.spydus.co.uk", username=username, password=password
     )
 
-    fieldsets = list(
-        get_all_books_in_default_list(
-            browser, base_url=base_url, username=username, password=password
+    default_list = browser.get_default_list()
+
+    books = list(
+        tqdm.tqdm(
+            browser.get_books_in_list(url=default_list["url"]),
+            total=default_list["count"],
         )
     )
 
-    books = [
-        book
-        for (_, book) in tqdm.tqdm(
-            concurrently(
-                lambda fs: get_book_info(fs, browser=browser, base_url=base_url),
-                fieldsets,
-            ),
-            total=len(fieldsets),
-        )
-    ]
+    data = {"generated_at": datetime.datetime.now().isoformat(), "books": books}
 
     with open("books.json", "w") as out_file:
-        out_file.write(
-            json.dumps(
-                {"generated_at": datetime.datetime.now().isoformat(), "books": books},
-                indent=2,
-                sort_keys=True,
-            )
-        )
+        out_file.write(json.dumps(data, indent=2, sort_keys=True))
