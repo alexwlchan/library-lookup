24ba2ded2f5ac2a135a99a2935e81cc3d0cfc18d
Alex Chan <alex@alexwlchan.net>
2023-11-09 08:39:37 +0000

    Add nice rendering of dates on the page

--- assets/library_lookup.js
+++ assets/library_lookup.js
@@ -182,3 +182,44 @@ document.querySelector('#selectedBranchCount').innerHTML = `(${selectedBranches.
     document.querySelector('#selectedBranchCount').innerHTML = '';
   }
 }
+
+// Renders a date in the local timezone, including day of the week.
+// e.g. "Fri, 22 May 2020"
+const dateFormatter = new Intl.DateTimeFormat(
+  [], {"year": "numeric", "month": "long", "day": "numeric", "weekday": "short"}
+)
+
+// Renders an HH:MM time in the local timezone, including timezone info.
+// e.g. "12:17 BST"
+const timeFormatter = new Intl.DateTimeFormat(
+  [], {"hour": "numeric", "minute": "numeric"}
+)
+
+// Given an ISO 8601 date string, render it as a more friendly date
+// in the user's timezone.
+//
+// Examples:
+// - "today @ 12:00 BST"
+// - "yesterday @ 11:00 CST"
+// - "Fri, 22 May 2020 @ 10:00 PST"
+//
+// From https://alexwlchan.net/2020/human-friendly-dates-in-javascript/
+//
+function getHumanFriendlyDateString(iso8601_date_string) {
+  const date = new Date(Date.parse(iso8601_date_string));
+
+  // When are today and yesterday?
+  const today = new Date();
+  const yesterday = new Date().setDate(today.getDate() - 1);
+
+  // We have to compare the *formatted* dates rather than the actual dates --
+  // for example, if the UTC date and the localised date fall on either side
+  // of midnight.
+  if (dateFormatter.format(date) == dateFormatter.format(today)) {
+    return "today at " + timeFormatter.format(date);
+  } else if (dateFormatter.format(date) == dateFormatter.format(yesterday)) {
+    return "yesterday at " + timeFormatter.format(date);
+  } else {
+    return dateFormatter.format(date) + " at " + timeFormatter.format(date);
+  }
+}
