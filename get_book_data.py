6022c5d4df1817855f496312e7e54f2b6fae287b
Alex Chan <alex@alexwlchan.net>
2024-05-16 17:20:01 +0100

    Fix another type-related issue

--- get_book_data.py
+++ get_book_data.py
@@ -292,9 +292,7 @@ class LibraryBrowser:
 
         soup = self._get_soup(availability_url)
 
-        availability = parse_availability_info(
-            typing.cast(bs4.BeautifulSoup, soup._underlying)
-        )
+        availability = parse_availability_info(soup)
 
         return {
             "title": title,
