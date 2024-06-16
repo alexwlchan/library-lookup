a3d0528ee5491c3fb5cb088204c7f8e803d1fad5
Alex Chan <alex@alexwlchan.net>
2024-06-16 20:07:22 +0100

    Fix a couple of typing issues

--- get_book_data.py
+++ get_book_data.py
@@ -328,14 +328,14 @@ class LibraryBrowser:
 
 if __name__ == "__main__":
     try:
-        username = os.environ["LIBRARY_CARD_NUMBER"]
-        password = os.environ["LIBRARY_CARD_PASSWORD"]
+        username: str | None = os.environ["LIBRARY_CARD_NUMBER"]
+        password: str | None = os.environ["LIBRARY_CARD_PASSWORD"]
     except KeyError:
         username = keyring.get_password("library", "username")
-        assert username is not None, "Could not get username from keychain!"
-
         password = keyring.get_password("library", "password")
-        assert password is not None, "Could not get password from keychain!"
+
+    assert username is not None, "Could not get username!"
+    assert password is not None, "Could not get password!"
 
     browser = LibraryBrowser(
         base_url="https://herts.spydus.co.uk", username=username, password=password
